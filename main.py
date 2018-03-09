c43161d5dbec7c1e93bb79d7dd286f3773268ac2
Mohammadreza Baharani <mbaharan@uncc.edu>
2018-03-09 15:32:09 -0500

    Now it working. I fixed some bugs. Next step is to create a wiki page for installing other required open-source tools.

--- main.py
+++ main.py
@@ -14,84 +14,10 @@ from capstone import CS_ARCH_X86, CS_MODE_64, CS_MODE_ARM, CS_MODE_V8, CS_ARCH_A
 import time
 import matplotlib.pyplot as plt
 import logging
-import argparse
-import functools
-import os
-
-
-def readable_dir(prospective_dir):
-    if not os.path.isdir(prospective_dir):
-        raise Exception("readable_dir:{0} is not a valid path".format(prospective_dir))
-    if os.access(prospective_dir, os.R_OK):
-        return prospective_dir
-    else:
-        raise Exception("readable_dir:{0} is not a readable dir".format(prospective_dir))
-
-
-def check_range(value, min_val, max_val):
-    try:
-        f_value = float(value)
-    except ValueError as err:
-        raise argparse.ArgumentTypeError(str(err))
-
-    if not min_val < f_value <= max_val:
-        raise argparse.ArgumentTypeError("%s is not a in the range of (0, 1].".format(value))
-    return f_value
-
-
-def check_window_size(value):
-    nums = value.split(',')
-
-    try:
-        min_w = int(nums[0])
-        max_w = int(nums[1])
-    except ValueError as err:
-        raise argparse.ArgumentTypeError(str(err))
-
-    if min_w >= max_w:
-        raise argparse.ArgumentTypeError("{} should be less than {}". format(min_w, max_w))
-
-
-def get_window_sizes(value):
-    nums = value.split(',')
-    min_w = int(nums[0])
-    max_w = int(nums[1])
-
-    return [min_w, max_w]
-
-
-def extant_file(value):
-    """
-    'Type' for argparse - checks that file exists but does not open.
-    """
-    if not os.path.exists(value):
-        # Argparse uses the ArgumentTypeError to give a rejection message like:
-        # error: argument input: x does not exist
-        raise argparse.ArgumentTypeError("{0} does not exist".format(value))
-    return value
-
-
-check_coverage = functools.partial(check_range, min=0, max=1)
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--trace_file", help="Trace file in sqlite database format. Please refer to PyArmTracer project for \n"
-                                "more information.", type=extant_file)
-parser.add_argument("-c", help="Coverage percent of trace file. A float number between (0, 1].",
-                    type=check_coverage)
-
-parser.add_argument("-w", help="Window size range. For example --w=5,7 means that windows size will change\n"
-                                " from 5 to 7.")
-parser.add_argument("-r", "--result_directory", help="A directory path for saving log files and results.",
-                    type=readable_dir)
-
-parser.add_argument("-n", "--application_name", help="Application name.")
-
-parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
-args = parser.parse_args()
-
 
 if __name__ == "__main__":
+
+    from PyMicroTracer.ArgumentsChecker import args, get_window_sizes
 
     ins_line_style = ['b:']
     hybrid_line_style = ['r:']
@@ -99,12 +25,12 @@ if __name__ == "__main__":
     max_scheduled_inst_hbb = ['ko']
     max_scheduled_inst_sbp = ['kx']
 
-    [min_w, max_w] = get_window_sizes(args.w)
+    [min_w, max_w] = get_window_sizes(args.window_size)
 
     powers = [i for i in range(min_w, max_w+1)]
     window_sizes = [2**power for power in powers]
 
-    coverage = float(args.c)
+    coverage = float(args.coverage)
     batch_size = 2*max(powers)
 
     fig_axis = plt.subplots()
@@ -121,15 +47,19 @@ if __name__ == "__main__":
     print('Working on: {}'.format(args.application_name))
     dict_per_bnch={}
     frmt = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
-    logging.basicConfig(format=frmt, filename=args.result_directory+args.application_name+'_info.log', level=logging.INFO)
-    logging.basicConfig(format=frmt, filename=args.result_directory+args.application_name+'_warn.log', level=logging.WARN)
-    logging.basicConfig(format=frmt, filename=args.result_directory+args.application_name+'_err.log', level=logging.ERROR)
-    logging.basicConfig(format=frmt, filename=args.result_directory+args.application_name+'_debug.log', level=logging.DEBUG)
+    logging.basicConfig(format=frmt, filename=args.result_directory+args.application_name+'_info.log',
+                        level=logging.INFO)
+    logging.basicConfig(format=frmt, filename=args.result_directory+args.application_name+'_warn.log',
+                        level=logging.WARN)
+    logging.basicConfig(format=frmt, filename=args.result_directory+args.application_name+'_err.log',
+                        level=logging.ERROR)
+    logging.basicConfig(format=frmt, filename=args.result_directory+args.application_name+'_debug.log',
+                        level=logging.DEBUG)
 
     dp = DifferentialProcessor(db_file_name=args.trace_file, machine_mode=machine_mode, machine_arch=machine_arch,
                                log_handler=logging,  batch_size=batch_size, how_many_iteration=-1,
-                               prefix_dir=args.result_directory, draw_dependency_graphs=False, app_name=args.application_name,
-                               log_output=False)
+                               prefix_dir=args.result_directory, draw_dependency_graphs=args.draw_dependency_graph,
+                               app_name=args.application_name, log_output=False)
 
     whole_ipc = dp.simulate_uniform(window_sizes=window_sizes, coverage=coverage)
 
@@ -142,17 +72,17 @@ if __name__ == "__main__":
     print(x2_tick_labels)
 
     plot_me(x=window_sizes, y=whole_ipc[0], yScale=y_scale, xTicks=window_sizes, xTickLabels=x_tick_labels,
-            xLabel='Window Size', yLabel='IPC', figAxis=fig_axis, style=hybrid_line_style[idx],
+            xLabel='Window Size', yLabel='IPC', figAxis=fig_axis, style=hybrid_line_style[0],
             label=args.application_name+" hyprid-bbl", legenLoc="upper left", x2=whole_ipc[5],
             x2TickLabels=x2_tick_labels, x2Ticks=whole_ipc[5])
 
     plot_me(x=window_sizes, y=whole_ipc[1], yScale=y_scale, xTicks=window_sizes, xTickLabels=x_tick_labels,
-            xLabel='Window Size', yLabel='IPC', figAxis=fig_axis, style=ins_line_style[idx],
+            xLabel='Window Size', yLabel='IPC', figAxis=fig_axis, style=ins_line_style[0],
             label=args.application_name+" super-bbl", legenLoc="upper left", x2=whole_ipc[5],
             x2TickLabels=x2_tick_labels, x2Ticks=whole_ipc[5])
 
     plot_me(x=window_sizes, y=whole_ipc[2], yScale=y_scale, xTicks=window_sizes, xTickLabels=x_tick_labels,
-            xLabel='Window Size', yLabel='IPC', figAxis=fig_axis, style=static_line_style[idx],
+            xLabel='Window Size', yLabel='IPC', figAxis=fig_axis, style=static_line_style[0],
             label=args.application_name + " static-bbl", legenLoc="upper left", x2=whole_ipc[5],
             x2TickLabels=x2_tick_labels, x2Ticks=whole_ipc[5])
 
